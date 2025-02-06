67023b3ab4648cbdb4d92b1c3ea6c58cf7cda1a3
Aditya Thakur <aditya85thakur@gmail.com>
2025-02-06 11:39:52 +0530

    changed Quote.tsx

--- frontend/src/components/Quote.tsx
+++ frontend/src/components/Quote.tsx
@@ -3,13 +3,14 @@ export const Quote = () => {
         <div className="flex justify-center">
             <div className="max-w-lg">
                 <div className="text-3xl font-bold">
-                    "The customer support I received was exceptional. The support team went above and beyond to address my concerns"
+                    "When in doubt,
+                    look intelligent."
                 </div>
                 <div className="max-w-md text-xl font-semibold text-left mt-4">
-                    Julies Winfield
+                    Garrison Keillor
                 </div>
                 <div className="max-w-md text-sm font-light text-slate-400">
-                    CEO | Acme corp
+                    Author | Lake Wobegon Days
                 </div>
             </div>
         </div>
