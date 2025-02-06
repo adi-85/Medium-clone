1b24bb7d4ac5c59c9c245d2cdc28374b9af30705
Aditya Thakur <aditya85thakur@gmail.com>
2025-02-06 12:48:25 +0530

    added cors middleware

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -14,15 +14,29 @@ const app = new Hono<{
 	}
 }>();
 
-app.use(
-	"*",
-	cors({
-	  origin: "https://medium-clone-psi-two.vercel.app", 
-	  allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-	  allowHeaders: ["Content-Type", "Authorization"],
-	  credentials: true, 
-	})
-  );
+// app.use(
+// 	"*",
+// 	cors({
+// 	  origin: "https://medium-clone-psi-two.vercel.app", 
+// 	  allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+// 	  allowHeaders: ["Content-Type", "Authorization"],
+// 	  credentials: true, 
+// 	})
+//   );
+
+app.use('*', (ctx, next) => {
+	// Manually add the Access-Control-Allow-Origin header for preflight requests
+	ctx.header('Access-Control-Allow-Origin', 'https://medium-clone-psi-two.vercel.app');
+	ctx.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+	ctx.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+	ctx.header('Access-Control-Allow-Credentials', 'true');
+	
+	
+  
+	return next();
+  });
+
+
 
 app.route("/api/v1/user", userRouter);
 app.route("/api/v1/blog", blogRouter);
